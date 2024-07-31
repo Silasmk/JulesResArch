7fd92859dbc4545f260e89be077d1f4b87b31bf1
Silasmk <qwouzi@gmail.com>
2024-07-31 14:05:14 +0200

    h

--- script.js
+++ script.js
@@ -1,28 +1,81 @@
-function upload() {
-    document.getElementById("input_link").innerHTML = "Changed";
+var videos = [
+    { id: 'n3RprPpVp-g', startTime: 1189 },
+    { id: 'M7lc1UVf-VE', startTime: 60 },
+    { id: '39QXz1bbWxw', startTime: 48 },
+    { id: 'bdBwTGJrD6c', startTime: 1670 },
+    { id: 'pcuv0RubURo', startTime: 600 },
+    { id: 'imq2XbWZwRc', startTime: 60 },
+    { id: 'FTIdLXifKO0', startTime: 10 }
+];
+
+var players = [];
+
+function createVideoContainers() {
+    var containerWrapper = document.getElementById('video-container-wrapper');
+
+    if (!containerWrapper) {
+        return;
+    }
+
+    containerWrapper.innerHTML = '';
+
+    videos.forEach((video, index) => {
+        var div = document.createElement("div");
+        div.className = "video-container";
+        div.id = `player${index + 1}`;
+        containerWrapper.appendChild(div);
+    });
 }
-var player;
 
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player('youtube-video', {
-                events: {
-                    'onReady': onPlayerReady
-                },
-                playerVars: {
-                    'start': YOUR_START_TIME,
-                    'autoplay': 1,
-                    'controls': 0
-                }
-            });
-        }
+function loadYouTubeAPI() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
 
-        function onPlayerReady(event) {
-            event.target.seekTo(1189, true);
-            event.target.pauseVideo();
-        }
+function initializeYouTubePlayers() {
+    videos.forEach((video, index) => {
+        players[index] = new YT.Player(`player${index + 1}`, {
+            height: '180',
+            width: '320',
+            videoId: video.id,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    });
+}
 
-        // Load the IFrame Player API code asynchronously.
-        var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onYouTubeIframeAPIReady() {
+    initializeYouTubePlayers();
+}
+
+function onPlayerReady(event) {
+    var player = event.target;
+    var playerIndex = players.indexOf(player);
+    var videoData = videos[playerIndex];
+
+
+    // Seek to the start time
+    player.seekTo(videoData.startTime, true);
+    // Mute the video
+    player.mute();
+    // Start playing the video
+    player.playVideo();
+}
+
+// This function is called when any player's state changes
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        // Pause the video immediately
+        event.target.pauseVideo();
+    }
+}
+
+// Initialize video containers and load the YouTube API
+window.onload = function() {
+    createVideoContainers();
+    loadYouTubeAPI();
+};
